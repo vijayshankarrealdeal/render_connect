8be10f9542e268fb9d348162c8312ed8d5730e25
Vijay Shankar <54475842+vijayshankarrealdeal@users.noreply.github.com>
2025-02-07 20:22:06 +0000

    directly add table

--- main.py
+++ main.py
@@ -19,8 +19,14 @@ def read_root(db: Session = Depends(get_db)):
     return {"server_time": str(result[0])}
 
 @app.get("/create_table")
-def create_table(query: str, db: Session = Depends(get_db)):
-    query = sql_text(query)
+def create_table(db: Session = Depends(get_db)):
+    query = sql_text("""CREATE TABLE meme (id SERIAL PRIMARY KEY,         
+                     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,         
+                     post_name VARCHAR(255) NOT NULL,         
+                     image_name VARCHAR(255) NOT NULL,        
+                     download_url TEXT NOT NULL,         
+                     caption TEXT NOT NULL,         
+                     image_text TEXT NOT NULL,);""")
     result = db.execute(query).fetchone()
     return {"res":result}
 
